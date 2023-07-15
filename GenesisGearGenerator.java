e987ab4818b3a01d171c0c2b4568a3280b334081
Electross <jarrettdh24@gmail.com>
2023-07-14 22:14:50 -0700

    Removed extra space

--- GenesisGearGenerator.java
+++ GenesisGearGenerator.java
@@ -4,7 +4,6 @@ class GenesisGearGenerator
 {
     public static void main (String[] args) 
     {
-        
         GenesisGearGenerator guh = new GenesisGearGenerator();
         guh.output();
     }
